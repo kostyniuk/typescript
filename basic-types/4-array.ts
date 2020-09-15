dac9be5593660a1f80de1ad27b82f088c46f65bc
kostyniuk <kostyniuk7@gmail.com>
2020-09-15 09:39:00 +0300

    finished

---
+++ basic-types/4-array.ts
@@ -0,0 +1,5 @@
+let numbers: Array<number> = [1, 2, 3, 4, 5];
+
+//numbers.push('six'); //NOT WORKS - Argument of type '"six"' is not assignable to parameter of type 'number'
+
+console.log({ numbers });
