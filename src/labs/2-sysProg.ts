b965b424deab98031357bfd807a015a1549c9033
Alex Kostyniuk <kostyniuk7@gmail.com>
2020-10-05 14:35:39 +0300

    almost finished create()

--- src/labs/2-sysProg.ts
+++ src/labs/2-sysProg.ts
@@ -11,6 +11,10 @@ readline.question[util.promisify.custom] = (question: string) => {
   });
 };
 
+const getRandomIntRange = (min: number, max: number): number => {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
 type FileType = 'ordinar' | 'directory'
 
 interface IBlock {
@@ -54,6 +58,7 @@ interface IFileSystem {
   maxDescriptorsNum: number
   memory: IBlock[]
   bitMap: Number[]
+  files: IFile[]
 }
 
 class FileSystem implements IFileSystem {
@@ -63,7 +68,7 @@ class FileSystem implements IFileSystem {
   maxDescriptorsNum: number
   memory: IBlock[] = []
   bitMap: Number[]
-
+  files: IFile[] = []
 
   constructor(size: number, blockSize: number, maxDescriptorsNum: number) {
     this.size = size;
@@ -80,23 +85,91 @@ class FileSystem implements IFileSystem {
     }
   }
 
+  createFile(name: string): void {
+    const size = getRandomIntRange(1, 4 * this.blockSize) // selected 4 just for testing purposes
+    const blocks = Math.ceil(size / this.blockSize);
+    for (let i = 0; i < blocks; i++) {
+      const index = getRandomIntRange(0, this.blocksQuantity)
+      this.memory[index] = this.generateData()
+      this.bitMap[index] = 1;
+      console.log({ size, blocks, index, bits: this.generateData() })
+    }
+    console.log(name)
+  }
+
+  private generateData(): IBlock {
+    let block: IBlock = { bits: (new Array(this.blockSize)).fill(0) };
+    block.bits = block.bits.map(_ => getRandomIntRange(0, 9))
+    return block;
+  }
+}
+
+const handleMultiCommands = (str: string): [string[], string] => {
+  console.log({ str })
+
+  const [command, ...params] = str.split(' ')
+
+  return [params, command]
 }
 
 (async () => {
 
-  const fs = new FileSystem(64, 4, 15)
+  let mounted = false
 
-  console.log({ fs, memory: fs.memory[0] })
+  let fs: FileSystem | null;
 
   let exit = false
 
   while (!exit) {
-    const answer: string = await util.promisify(readline.question)('> ');
+    let params: string[] = [];
+    let answer: string = await util.promisify(readline.question)('> ');
+
+    if (answer.split(' ').length > 1) {
+      [params, answer] = handleMultiCommands(answer)
+    }
+
+    console.log({ answer, params })
 
     switch (answer) {
 
       case 'mount': {
-        console.log('File system connected');
+        if (mounted) {
+          console.log('File system is already mounted');
+        } else {
+          fs = new FileSystem(64, 4, 15);
+          mounted = true;
+          console.log('File system mounted');
+          console.log({ fs, memory: fs.memory[0] })
+        }
+        break;
+      }
+
+      case 'unmount': {
+        if (mounted) {
+          fs = null;
+          mounted = false;
+          console.log('File system unmounted');
+          console.log({ fs })
+        } else {
+          console.log('File system isn\'t mounted');
+        }
+        break;
+      }
+
+      case 'create': {
+        const name = params[0]
+        if (!name) {
+          console.log('ERROR: No name provided')
+          break;
+        }
+
+        if (fs!) {
+          fs!.createFile(name)
+          console.log('CREATING FILE', name)
+
+        } else {
+          console.log('ERROR: File system isn\'t mounted')
+        }
         break;
       }
 
@@ -106,6 +179,8 @@ class FileSystem implements IFileSystem {
         break;
 
       default:
+        console.log(fs!)
+        fs!.memory.map(el => console.log(el))
         console.log('Unknown command')
     }
 
